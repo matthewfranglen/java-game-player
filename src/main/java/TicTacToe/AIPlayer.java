62f9ddb6ee5650e709af3784ea353a8da4db5eb8
Alessandra Williams-Bellotti <alessandra@brandwatch.com>
2016-05-18 14:55:32 +0100

    split minimax, create move class, and return map instead of array

--- src/main/java/TicTacToe/AIPlayer.java
+++ src/main/java/TicTacToe/AIPlayer.java
@@ -1,44 +1,74 @@
 package TicTacToe;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AIPlayer {
 
-    public int[] miniMax(Board board, int depth, String currentPlayer, Game game) {
+    public Map<Move, Integer> maximize(Board board, int depth, Game game) {
         Cell[][] cells = board.getBoard().clone();
-        List<int[]> possibleMoves = getEmptySlots(board);
-        int bestScore = currentPlayer.equals(game.getAiPlayerPiece()) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        List<Move> possibleMoves = getEmptySlots(board);
+        int bestScore = Integer.MIN_VALUE;
         int currentScore;
         int bestRow = -1;
         int bestCol = -1;
-        if (WinChecker.isAWinForAIPlayer(board, game.getAiPlayerPiece())
-                || WinChecker.isAWinForOpponent(board, game.getOpponentPiece())
-                || TieChecker.isATie(board)
-                || depth == 0) {
+        if (gameIsOver(board, game) || depth == 0) {
             bestScore = getScore(board, game);
         } else {
-            for (int[] move : possibleMoves) {
-                cells[move[0]][move[1]].setCell(currentPlayer);
-                if (currentPlayer.equals(game.getAiPlayerPiece())) {
-                    currentScore = miniMax(board, depth - 1, game.getOpponentPiece(), game)[0];
-                    if (currentScore > bestScore) {
-                        bestScore = currentScore;
-                        bestRow = move[0];
-                        bestCol = move[1];
-                    }
-                } else {
-                    currentScore = miniMax(board, depth - 1, game.getAiPlayerPiece(), game)[0];
-                    if (currentScore < bestScore) {
-                        bestScore = currentScore;
-                        bestRow = move[0];
-                        bestCol = move[1];
-                    }
+            for (Move move : possibleMoves) {
+                cells[move.getRow()][move.getColumn()].setCell(game.getAiPlayerPiece());
+                Map.Entry<Move, Integer> entry = minimize(board, depth - 1, game).entrySet().iterator().next();
+                currentScore = entry.getValue();
+                if (currentScore > bestScore) {
+                    bestScore = currentScore;
+                    bestRow = move.getRow();
+                    bestCol = move.getColumn();
                 }
-                cells[move[0]][move[1]].setCell("[ ]");
+                cells[move.getRow()][move.getColumn()].setCell("[ ]");
             }
         }
-        return new int [] {bestScore, bestRow, bestCol};
+        Move move = new Move(bestRow, bestCol);
+        Map<Move, Integer> moveAndScore = new HashMap<>();
+        moveAndScore.put(move, bestScore);
+
+        return moveAndScore;
+    }
+
+    private Map<Move, Integer> minimize(Board board, int depth, Game game) {
+        Cell[][] cells = board.getBoard().clone();
+        List<Move> possibleMoves = getEmptySlots(board);
+        int bestScore = Integer.MAX_VALUE;
+        int currentScore;
+        int bestRow = -1;
+        int bestCol = -1;
+        if (gameIsOver(board, game) || depth == 0) {
+            bestScore = getScore(board, game);
+        } else {
+            for (Move move : possibleMoves) {
+                cells[move.getRow()][move.getColumn()].setCell(game.getOpponentPiece());
+                Map.Entry<Move, Integer> entry = maximize(board, depth - 1, game).entrySet().iterator().next();
+                currentScore = entry.getValue();
+                if (currentScore < bestScore) {
+                    bestScore = currentScore;
+                    bestRow = move.getRow();
+                    bestCol = move.getColumn();
+                }
+                cells[move.getRow()][move.getColumn()].setCell("[ ]");
+            }
+        }
+        Move move = new Move(bestRow, bestCol);
+        Map<Move, Integer> moveAndScore = new HashMap<>();
+        moveAndScore.put(move, bestScore);
+
+        return moveAndScore;
+    }
+
+    private boolean gameIsOver(Board board, Game game) {
+        return WinChecker.isAWinForAIPlayer(board, game.getAiPlayerPiece())
+                || WinChecker.isAWinForOpponent(board, game.getOpponentPiece())
+                || TieChecker.isATie(board);
     }
 
     private static int getScore(Board board, Game game) {
@@ -53,15 +83,13 @@ public class AIPlayer {
         return score;
     }
 
-    private static List<int[]> getEmptySlots(Board board){
-        List<int[]> possibleMoves = new ArrayList<>();
+    private static List<Move> getEmptySlots(Board board){
+        List<Move> possibleMoves = new ArrayList<>();
         Cell[][] cells = board.getBoard();
         for (int row = 0; row < cells.length; row++) {
             for (int column = 0; column < cells.length; column++) {
                 if(cells[row][column].getCell().equals("[ ]")) {
-                    int[] move = new int[2];
-                    move[0] = row;
-                    move[1] = column;
+                    Move move = new Move(row, column);
                     possibleMoves.add(move);
                 }
             }
--- src/main/java/TicTacToe/Run.java
+++ src/main/java/TicTacToe/Run.java
@@ -3,6 +3,7 @@ package TicTacToe;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 public class Run {
 
@@ -106,14 +107,13 @@ public class Run {
                 "(eg the first cell is 00, the one to it's right is 01, the one below it is 10: ");
         try {
             String input = br.readLine();
-            int i = Integer.parseInt(input.substring(0, 1));
-            int j = Integer.parseInt(input.substring(1, 2));
+            Move move = new Move(Integer.parseInt(input.substring(0, 1)), Integer.parseInt(input.substring(1, 2)));
             Cell[][] currentBoard = board.getBoard();
-            if (!currentBoard[i][j].getCell().equals("[ ]")) {
+            if (!currentBoard[move.getRow()][move.getColumn()].getCell().equals("[ ]")) {
                 System.out.println("Already a piece there. Try again\n");
                 getMove(board, game);
             } else {
-                currentBoard[i][j].setCell(game.getOpponentPiece());
+                currentBoard[move.getRow()][move.getColumn()].setCell(game.getOpponentPiece());
                 board.setBoard(currentBoard);
                 board.printBoard();
             }
@@ -126,15 +126,15 @@ public class Run {
     private static void makeMove(Board board, Game game) {
         System.out.println("AI's move:");
         AIPlayer aiPlayer = new AIPlayer();
-        int[] playerMove = aiPlayer.miniMax(board, game.getDifficultyLevel(), game.getAiPlayerPiece(), game);
-        int i = playerMove[1];
-        int j = playerMove[2];
+        Map<Move, Integer> moveAndScore = aiPlayer.maximize(board, game.getDifficultyLevel(), game);
+        Map.Entry<Move, Integer> moveAndScoreEntry = moveAndScore.entrySet().iterator().next();
+        Move move = moveAndScoreEntry.getKey();
 
         Cell[][] currentBoard = board.getBoard();
-        if (!currentBoard[i][j].getCell().equals("[ ]")) {
+        if (!currentBoard[move.getRow()][move.getColumn()].getCell().equals("[ ]")) {
             makeMove(board, game);
         } else {
-            currentBoard[i][j].setCell(game.getAiPlayerPiece());
+            currentBoard[move.getRow()][move.getColumn()].setCell(game.getAiPlayerPiece());
             board.setBoard(currentBoard);
             board.printBoard();
         }
